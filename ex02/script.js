3d270d056cf0eb225bcc12e84b44fba4cc6290be
I1HSD <arthursudario.10@gmail.com>
2026-04-01 10:08:36 -0300

    Mior ou menor de idade

---
+++ ex02/script.js
@@ -0,0 +1,10 @@
+let idade = Number(prompt("Digite o número"));
+   if (idade >= 18){
+      alert("Você é maior de idade")
+      }else {
+   if (idade <18){
+      alert("Você é menor de idade")
+      }else {
+
+      }
+   }
