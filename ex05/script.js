99f7d073e9647985a69f424e1e0b9f8ee3f03a1a
I1HSD <arthursudario.10@gmail.com>
2026-04-01 10:13:38 -0300

    Número múltiplo de 5

---
+++ ex05/script.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Digite um número"));
+
+numero = Number(numero);
+
+   if (numero % 5 === 0) {
+      alert("O número é múltiplo de 5.");
+   } else {
+      alert("O número não é múltiplo de 5");
+   }
