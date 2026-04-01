46fdf3abef277ca7a8c7ce3d54ffbeb8573b67bc
I1HSD <arthursudario.10@gmail.com>
2026-04-01 10:10:30 -0300

    Número par e ímpar

---
+++ ex03/script.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Digite o número"));
+
+numero = Number(numero);
+
+   if (numero % 2 === 0) {
+      alert("O número é par.");
+   } else {
+      alert("O número é ímpar");
+   }
